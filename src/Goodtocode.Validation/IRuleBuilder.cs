2f972195d54576cb60bb48bfdc7751a58d5ae19b
Robert Good <robert.good@goodtocode.com>
2026-02-16 21:10:55 -0800

    interface segregation

--- src/Goodtocode.Validation/IRuleBuilder.cs
+++ src/Goodtocode.Validation/IRuleBuilder.cs
@@ -1,11 +1,15 @@
 ﻿namespace Goodtocode.Validation;
 
-public interface IRuleBuilder<T, TProperty>
+public interface IRuleBuilder<T, TProp>
 {
-    IRuleBuilder<T, TProperty> NotEmpty(string? errorMessage = null);
-    IRuleBuilder<T, TProperty> LessThanOrEqualTo(Func<T, TProperty> other, string? errorMessage = null);
-    IRuleBuilder<T, TProperty> GreaterThanOrEqualTo(Func<T, TProperty> other, string? errorMessage = null);
+    IRuleBuilder<T, TProp> NotEqual(TProp other, string? errorMessage = null);
+    IRuleBuilder<T, TProp> Equal(TProp other, string? errorMessage = null);
+    IRuleBuilder<T, TProp> IsInEnum(string? errorMessage = null);
+    IRuleBuilder<T, TProp> NotEmpty(string? errorMessage = null);
+    IRuleBuilder<T, TProp> LessThanOrEqualTo(Func<T, TProp> other, string? errorMessage = null);
+    IRuleBuilder<T, TProp> GreaterThanOrEqualTo(Func<T, TProp> other, string? errorMessage = null);
 #pragma warning disable CA1716 // Identifiers should not match keywords
-    IRuleBuilder<T, TProperty> When(Func<T, bool> predicate, string? errorMessage = null);
+    IRuleBuilder<T, TProp> When(Func<T, bool> predicate);
 #pragma warning restore CA1716
+    IRuleBuilder<T, TProp> Must(Func<T, bool> predicate, string? errorMessage = null);
 }
